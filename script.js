4b18e53200d5f9c347af8956b7c949595dac8a65
Gabriel Prata <119508139+gabrielmprata@users.noreply.github.com>
2026-06-12 14:25:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener("scroll", () => {
+
+  const cards = document.querySelectorAll(".info-card");
+
+  cards.forEach((card) => {
+
+    const top = card.getBoundingClientRect().top;
+
+    if(top < window.innerHeight - 100){
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+
+  });
+
+});
